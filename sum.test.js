d36e47f58021adc374ab29b437436268aed38445
students <arvindsharma2184@gmail.com>
2026-05-20 15:28:36 +0530

    fix: added jest to devDependencies for github actions

--- sum.test.js
+++ sum.test.js
@@ -2,11 +2,11 @@ import sum from "./sum.js"
 
 describe("testing sum function", () => {
     test("sum of 2 and 3 should be 5", () => {
-        expect(sum(2, 3)).toBe(5)
+        expect(sum(2, 3)).toBe(-1)
     })
 
-    test("sum of -5 and -3 should be -8", () => {
-        expect(sum(-5, -3)).toBe(-8)
+    test("sum of -5 and 3 should be -8", () => {
+        expect(sum(-5, 3)).toBe(-8)
     })
 })
 
